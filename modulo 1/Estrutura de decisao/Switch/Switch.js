c3aa1bb22891e5d56c7ebb2e9a629c8a490af988
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-05-18 15:50:32 -0300

    add

--- modulo 1/Estrutura de decisao/Switch/Switch.js
+++ modulo 1/Estrutura de decisao/Switch/Switch.js
@@ -31,3 +31,46 @@ function cardapio(ds) {
 
 //-------------------------------------------------------------------------------------------------------------------
 
+var agora = new Date()
+var diaSem = agora.getDay() //para pegar o dia da semana
+//todos os dias da semana são enumeradas da seguinte forma
+/*
+ 0=Domingo
+ 1=Segunda
+ 2=Terça
+ 3=Quarta
+ 4=Quinta
+ 5=Sexta
+ 6=Sabado
+*/ 
+
+//console.log(diaSem)
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log('[ERRO] Dia invalido')
+        break
+    }
+
+    //switch é muito importante para testar datos pontuais ( ele so funciona com numeros inteiros e caracteres)
---
+++ modulo 1/Estrutura de decisao/while/while.js
@@ -0,0 +1,38 @@
+//repetições 
+
+var c = 1
+do { // do = faça
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6) { //while = enquanto
+   console.log(`Passo ${c}`)
+   c++ //c = c + 1 
+
+} */
+
+//-------------------------------------------------------------------------------------------------------------------
+
+//Repetições 
+
+// for (inicio; teste; incr) {   
+
+  // }
+
+  console.log('Vai começar...')
+  for (var c=1; c<=4; c++) {
+      console.log(c)
+  }
+  console.log('FIM!')
+  
+  // mesma coisa do que 
+  /* 
+  var c = 1 
+  while ( c <= 11) {
+      console.log(`passo ${c}`)
+      c++
+  }
+  */
